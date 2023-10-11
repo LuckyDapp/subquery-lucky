b5e82fc7b677a9bddde2b72189df8347d7492d1f
GuiGou <guigoucrypto@gmail.com>
2023-10-11 14:07:08 +0200

    Prepare deployment on Astar

--- src/indexAstar.ts
+++ src/indexAstar.ts
@@ -1,7 +1,7 @@
 
 // ASTAR
 process.env.DAPPSTAKING_CONTRACT_ID = "";
-process.env.DAPPSTAKING_DEVELOPER_ID = "";
+process.env.DAPPSTAKING_DEVELOPER_ID = "ZRM5bUZYZrV3Vc2BUe5eHsbDj6v9FqNndBsvGBEAJHkCDC6";
 
 //Exports all handler functions
 export * from "./mappings/mappingHandlers";

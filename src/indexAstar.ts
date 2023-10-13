578771fbfba065ff3fd16149da4672f78d05b25d
GuiGou <guigoucrypto@gmail.com>
2023-10-13 18:25:33 +0200

    Use verified contracts by Patron of Brushfam

--- src/indexAstar.ts
+++ src/indexAstar.ts
@@ -1,7 +1,7 @@
 
 // ASTAR
-process.env.DAPPSTAKING_CONTRACT_ID = "";
-process.env.DAPPSTAKING_DEVELOPER_ID = "ZRM5bUZYZrV3Vc2BUe5eHsbDj6v9FqNndBsvGBEAJHkCDC6";
+process.env.DAPPSTAKING_CONTRACT_ID = "ZSV1GVepvmWFdshMWgczS4zYvmmwEsBjWQjN4WDpUEFRRPy";
+process.env.DAPPSTAKING_DEVELOPER_ID = "ZEUr1PBaxshhhPcF4jeVFVoC6BwCDYj48UsJ5ShquWN2yeE";
 
 //Exports all handler functions
 export * from "./mappings/mappingHandlers";
